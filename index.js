4b758fb4a64e427d5d00c7929c869a267d27c4ca
Adam <aberesdeak@autoscout24.com>
2015-05-18 17:15:42 +0200

    using port and ip for OpenShift

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var express = require('express');
 var app = express();
 app.use(express.static('./'));
 
-var server = app.listen(process.env.OPENSHIFT_NODEJS_PORT || 3000, function () {
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+var server = app.listen(port, ipaddress, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
